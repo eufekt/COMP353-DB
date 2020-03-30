3d7e50d7fd25cd0e0c9b9d9956732aae0194add5
Nichita Hariton <nichitahariton@hotmail.com>
2020-03-30 17:33:45 -0400

    more

--- create.php
+++ create.php
@@ -1,19 +1,46 @@
+<?php
+if (isset($_POST['submit'])) {
+    require "./config.php";
+
+    try  {
+        $connection = new PDO($dsn, $username, $password, $options);
+        
+        $new_user = array(
+            "id" => $_POST['id'],
+            "firstname" => $_POST['firstname'],
+            "lastname"  => $_POST['lastname'],
+            "email"     => $_POST['email']
+        );
+
+        $sql = sprintf(
+                "INSERT INTO %s (%s) values (%s)",
+                "hvc353_4.MyGuests",
+                implode(", ", array_keys($new_user)),
+                ":" . implode(", :", array_keys($new_user))                
+        );
+        
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+        
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
 <?php include "templates/header.php"; ?><h2>Add a user</h2>
 
     <form method="post">
+        <label for="id">Random id</label>
+    	<input type="text" name="id" id="id">
     	<label for="firstname">First Name</label>
     	<input type="text" name="firstname" id="firstname">
     	<label for="lastname">Last Name</label>
     	<input type="text" name="lastname" id="lastname">
     	<label for="email">Email Address</label>
     	<input type="text" name="email" id="email">
-    	<label for="age">Age</label>
-    	<input type="text" name="age" id="age">
-    	<label for="location">Location</label>
-    	<input type="text" name="location" id="location">
-    	<input type="submit" name="submit" value="Submit">
     </form>
 
-    <a href="index.php">Back to home</a>
+<a href="index.php">Back to home</a>
 
-    <?php include "templates/footer.php"; ?>
+<?php include "templates/footer.php"; ?>
--- install.php
+++ install.php
@@ -14,5 +14,4 @@ require "./config.php";
   }
   $connection = null;
 
-
 ?>
