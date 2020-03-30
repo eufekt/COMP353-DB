1070a441a3db0ba3be201b51338fd5d71431ccdf
Nichita Hariton <nichitahariton@hotmail.com>
2020-03-30 17:18:25 -0400

    more

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$host       = "hvc353.encs.concordia.ca";
+$username   = "hvc353_4";
+$password   = "technica";
+$dbname     = "hvc353_4"; 
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+?>
--- install.php
+++ install.php
@@ -1,26 +1,11 @@
 <?php
 
-$host       = "hvc353.encs.concordia.ca";
-$username   = "hvc353_4";
-$password   = "technica";
-$dbname     = "hvc353_4"; // will use later
-$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
-$options    = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-              );
+require "./config.php";
 
     try {
     $connection = new PDO($dsn, $username, $password, $options);
 
-    // $new_user = array(
-    // "firstname" => $_POST['firstname'],
-    // "lastname"  => $_POST['lastname'],
-    // "email"     => $_POST['email'],
-    // "age"       => $_POST['age'],
-    // "location"  => $_POST['location']
-    // );
-
-    $sql = "INSERT INTO hvc353_4.MyGuests (id, firstname, lastname, email) values (456, 'nichita', 'hariton', 'nh@hotmail.com')";
+    $sql = "INSERT INTO hvc353_4.MyGuests (id, firstname, lastname, email) values (756, 'nichita', 'hariton', 'nh@hotmail.com')";
 
     $connection->exec($sql);
 
