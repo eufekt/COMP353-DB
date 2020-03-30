78902f9829dffc2f859d6c0f32b2525c90193aa6
Nichita Hariton <nichitahariton@hotmail.com>
2020-03-30 17:58:52 -0400

    more

--- read.php
+++ read.php
@@ -1,13 +1,62 @@
-<?php include "templates/header.php"; ?>
+<?php
 
-    <h2>Find user based on location</h2>
+if (isset($_POST['submit'])) {
+  try {
+    require "./config.php";
 
-    <form method="post">
-    	<label for="location">Location</label>
-    	<input type="text" id="location" name="location">
-    	<input type="submit" name="submit" value="View Results">
-    </form>
+    $connection = new PDO($dsn, $username, $password, $options);
 
-    <a href="index.php">Back to home</a>
+    $sql = "SELECT *
+    FROM hvc353_4.MyGuests";
 
-<?php include "templates/footer.php"; ?>
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll();
+
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+}
+?>
+<?php require "templates/header.php"; ?>
+
+<?php
+if (isset($_POST['submit'])) {
+  if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+      <thead>
+        <tr>
+        <th>#</th>
+        <th>Id</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email Address</th>
+        </tr>
+      </thead>
+      <tbody>
+    <?php foreach ($result as $row) { ?>
+      <tr>
+            <td><?php echo escape($row["id"]); ?></td>
+            <td><?php echo escape($row["firstname"]); ?></td>
+            <td><?php echo escape($row["lastname"]); ?></td>
+            <td><?php echo escape($row["email"]); ?></td>
+      </tr>
+    <?php } ?>
+      </tbody>
+  </table>
+  <?php } else { ?>
+    > No results found for.
+  <?php }
+} ?>
+
+<h2>Find users</h2>
+
+<form method="post">
+  <input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
