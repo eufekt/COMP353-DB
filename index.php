c4706d43ea1efdb7e642e12b119ec8209bffdde1
Nichita Hariton <nichitahariton@hotmail.com>
2020-03-30 16:09:24 -0400

    follow another tutorial

--- index.php
+++ index.php
@@ -1,42 +1,12 @@
+<?php include "templates/header.php"; ?>
 
-<?php
-$dbname = "hvc353_4";
-$servername = "hvc353.encs.concordia.ca";
-$username = "hvc353_4";
-$password = "technica";
+<ul>
+  <li>
+    <a href="create.php"><strong>Create</strong></a> - add a user
+  </li>
+  <li>
+    <a href="read.php"><strong>Read</strong></a> - find a user
+  </li>
+</ul>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-//sql to create table
-
-//$sql = "CREATE TABLE MyGuests (
-//id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//firstname VARCHAR(30) NOT NULL,
-//lastname VARCHAR(30) NOT NULL,
-//email VARCHAR(50),
-//reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-//)";
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-echo "connection successful";
-
-
-
-//if ($conn->query($sql) === TRUE) {
-//  echo "Table MyGuests created successfully";
-//} else {
-//    echo "Error creating table: " . $conn->error;
-//}
-
-$conn->close();
-?>
- 
+<?php include "templates/footer.php"; ?>
---
+++ install.php
@@ -0,0 +1,22 @@
+<?php
+
+$host       = "hvc353.encs.concordia.ca";
+$username   = "hvc353_4";
+$password   = "technica";
+$dbname     = "hvc353_4"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+
+try {
+    $connection = new PDO("mysql:host=$host", $username, $password, $options);
+    // $sql = file_get_contents("data/init.sql");
+    // $connection->exec($sql);
+    // echo "Database and table users created successfully.";
+
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+
+?>
